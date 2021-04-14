d91597eddd114ffb923d0bae7fbb50cef103614e
IbrahimKhdairat <>
2021-04-14 11:52:48 +0000

    added a new updates

--- java.js
+++ java.js
@@ -1,1 +1,16 @@
-// alert('Welcome to my page');
+var username = prompt ('Welcome to my page , Please Enter Your Name !!');
+alert ('Welcom  ' + username);
+
+var age = prompt ( 'Please Enter Your Age  !!');
+
+if (age>=15)
+{
+  alert (' WELCOM :)'  );
+}
+
+else
+ {
+   
+   alert ('This content is dangerous , and we are not responsible for any injury caused by a wrong use of the content ....!!! ')
+
+ }
