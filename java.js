3191cc4f7e2f45eae96adc6558cbc11517377a8b
IbrahimKhdairat <>
2021-04-15 11:19:47 +0000

    updates on javascript file

--- java.js
+++ java.js
@@ -19,4 +19,34 @@ alert ('Welcome  ' + username);
 
 
   
- 
+ var motor = prompt ('Please Enter the type you want to see it (3 phase , 1 phase , dc )');
+
+while ( motor !=='3 phase' && motor !=='1 phase' && motor !=='dc')
+{
+   motor = prompt ('Please Enter the type you want to see it (3 phase , 1 phase , dc )');
+}
+
+var numofmotor = Number (prompt ('How many motor want to show ?'));
+
+for ( i = 1 ; i<=numofmotor ; i++)
+{
+
+if (motor=='3 phase')
+
+{
+  var image ='<img src="https://www.electrical4u.com/wp-content/uploads/What-is-a-3-Phase-Induction-Motor.png" alt ="motor"/>'
+  document.write(image);
+}
+
+else if (motor=='1 phase')
+{
+  var image ='<img src="https://image.made-in-china.com/2f0j00waLGCWKrYibQ/Single-Phase-Yc-Electric-Motor-7-5kw-10HP-220V.jpg" alt ="motor"/>'
+  document.write(image);
+}
+else if (motor=='dc')
+{
+var image ='<img src="https://www.wellpcb.com/sites/default/files/1_195.jpg" alt ="motor"/>'
+  document.write(image);
+}
+
+}
