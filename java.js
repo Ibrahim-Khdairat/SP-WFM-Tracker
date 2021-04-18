440073bacf14bf82b0fb274083d388a75722ab50
IbrahimKhdairat <>
2021-04-18 10:36:17 +0000

    Lab06

--- java.js
+++ java.js
@@ -1,6 +1,11 @@
-var username = prompt ('Welcome to my page , Please Enter Your Name !!');
+function userName()
+{
+ var name = prompt ('Welcome to my page , Please Enter Your Name !!');
+ return name;
+}
 
-alert ('Welcome  ' + username);
+alert ('Welcome  ' + userName());
+
 
  var age = prompt ( 'Please Enter Your Age  !!');
 
@@ -17,6 +22,40 @@ alert ('Welcome  ' + username);
 
   }
 
-
+function motor()
+{
+    var m = prompt ('Please Enter the type you want to see it (3 phase , 1 phase , dc )');
+    return m;
+}
   
- 
+var type =motor();
+
+while ( type !=='3 phase' && type !=='1 phase' && type !=='dc')
+{
+   motor();
+}
+
+var numofmotor = Number (prompt ('How many motor want to show ?'));
+
+for ( i = 1 ; i<=numofmotor ; i++)
+{
+
+if (type =='3 phase')
+
+{
+  var image ='<img src="https://www.electrical4u.com/wp-content/uploads/What-is-a-3-Phase-Induction-Motor.png" alt ="motor"/>'
+  document.write(image);
+}
+
+else if (type =='1 phase')
+{
+  var image ='<img src="https://image.made-in-china.com/2f0j00waLGCWKrYibQ/Single-Phase-Yc-Electric-Motor-7-5kw-10HP-220V.jpg" alt ="motor"/>'
+  document.write(image);
+}
+else if (type =='dc')
+{
+var image ='<img src="https://www.wellpcb.com/sites/default/files/1_195.jpg" alt ="motor"/>'
+  document.write(image);
+}
+
+}
